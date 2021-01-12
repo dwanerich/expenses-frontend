4ec13a242e70ac78062d47cde7b41e949e97260e
dwanerich <dwanerich@gmail.com>
2021-01-12 16:24:05 -0500

    imported packages redux, react-redux, thunk, react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/accounts', {
+      method: 'GET'
+    })
+    .then(r => r.json())
+    .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <h1>Expenses App</h1>
       </div>
     );
   }
