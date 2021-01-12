f79386acdebfd230e020c732750d8e0e8aba6570
dwanerich <dwanerich@gmail.com>
2021-01-12 18:11:19 -0500

    set up index.js file with store, composeEnhancers, redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore, applyMiddleware} from 'redux'
-import { thunk } from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'
+import reducer from './redux/reducer'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
+
+
+
 
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/redux/reducer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class reducer extends React.Component {
+    render() {
+        return (
+            <h1>build store here i guess</h1>
+        )
+    }
+}
